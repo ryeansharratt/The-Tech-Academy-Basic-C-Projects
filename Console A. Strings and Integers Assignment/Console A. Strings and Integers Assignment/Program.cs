b4251e9705b9bdeb93bc97f7653c0ba52dd6f1e1
ryeansharratt <ryean.ann@gmail.com>
2022-11-14 14:21:57 -0700

    Console A. Strings & Integers Assignment
    C# course, module 3 submission

---
+++ Console A. Strings and Integers Assignment/Console A. Strings and Integers Assignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_A.Strings_and_Integers_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listNumbers = new List<int>() //list of integers
+            {
+                20, 45, 10, 23, 15, 8, 16
+            };
+            Console.WriteLine("Type a number to divide all the numbers by:");
+
+            try
+            {
+                int userNumber = Convert.ToInt32(Console.ReadLine()); //user input
+                foreach (decimal number in listNumbers) //using decimal here and below to display result more precisely
+                {
+                    decimal result = number / userNumber; //declaring new variable of divided list integers by user input
+                    Console.WriteLine("Result is: " + result); //displays result 
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex) //handling is user inputs a word
+            {
+                Console.WriteLine("Error, have to input a number.");
+                return;
+            }
+            catch (DivideByZeroException ex) //handling a 0 input
+            {
+                Console.WriteLine("Please don't divide by 0.");
+            }
+            finally //runs no matter what
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine("Done with that block!");
+            Console.ReadLine();
+        }
+    }
+}
