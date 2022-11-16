32659c4f499a3f34117669cd8685752ed676d7d7
ryeansharratt <ryean.ann@gmail.com>
2022-11-15 21:31:20 -0700

    Calling Methods Assignment Submission
    C# course, module 4

---
+++ CallingMethodsAssignment1/CallingMethodsAssignment1/MathOperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment1
+{
+    public class MathOperation //these methods will take one number, and each operate on it together, one after the other
+                                //also int, so results will not be precise
+    {
+        public int Addition(int input) //defined method, 1 integer parameter of user's input
+        {
+            int result = input + 10; //addition, calculating result and storing in variable
+            return (result); //returning result to main program, to be returned again for next method...
+        }
+
+        public int Division(int input)
+        {
+            int result = input / 3; //division
+            return (result);
+        }
+
+        public int Multiply(int input)
+        {
+            return input * 2; //multiplication
+        }
+    }
+}
---
+++ CallingMethodsAssignment1/CallingMethodsAssignment1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:"); //user input
+            var userInput = Convert.ToInt32(Console.ReadLine()); //reading input, assigning to variable userInput
+
+            var mathOperator = new MathOperation(); //assigning the class MathOperation to var mathOperator
+            var result = mathOperator.Addition(userInput); //var result assigned to calling the Addition method from the class MathOperation (assigned var mathOperator), passing user input to method
+            Console.WriteLine(result); //displaying final total result; which will be the user's number after being put through each method
+            Console.ReadLine();
+            result = mathOperator.Division(userInput); //same but taking result from last method, calling Division method 
+            Console.WriteLine(result); //displaying final total result; which will be the user's number after being put through each method
+            Console.ReadLine();
+            result = mathOperator.Multiply(userInput); //same but taking result from last method, calling Multiply method to finish the math operations on the user's input
+            Console.WriteLine(result); //displaying final total result; which will be the user's number after being put through each method
+            Console.ReadLine();
+
+        }
+    }
+}
