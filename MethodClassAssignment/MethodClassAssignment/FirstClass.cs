731d81e73dd9a2b3abf292f9cbc0e8271bcb40d0
ryeansharratt <ryean.ann@gmail.com>
2022-11-16 19:38:04 -0700

    Method Class Assignment
    C# course, module 4

---
+++ MethodClassAssignment/MethodClassAssignment/FirstClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class FirstClass
+    {
+       public void Math(int num1, int num2)
+        {
+            int first = num1 + 12; //math operation on first int
+            Console.WriteLine(num2); //display 2nd to screen
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstClass firstClass = new FirstClass(); //instantiate class
+
+            firstClass.Math(20, 23); //call method, pass in two numbers
+            firstClass.Math(num1: 20, num2: 23); //call method, specify pars
+        }
+    }
+}
