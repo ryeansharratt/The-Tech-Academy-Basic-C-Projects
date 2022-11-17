d3925dcca9087e8ae1347512397368b145d0a1fc
ryeansharratt <ryean.ann@gmail.com>
2022-11-16 17:17:03 -0700

    Main Method Assignment Submission
    C# course, module 4 submission

---
+++ MainMethodAssignment/MainMethodAssignment/FirstClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class FirstClass        //methods of same name, but each taking in different parameter type
+    {
+        public int Math(int first) //takes an integer
+        {
+            int answer = first + 253; //performs addition
+            return answer; //returns answer
+        }
+
+        public int Math(decimal num1) //takes decimal
+        {
+            int answer = (int)(num1 / 4); //divides by 4
+            return answer; //return answer as int
+        }
+
+        public int Math(string string1) //takes a string
+        {
+            int num2 = Convert.ToInt32(string1);
+            int answer = num2 + 10;
+            return (answer); //return answer as int
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstClass firstClass = new FirstClass(); //instantiate class
+            Console.WriteLine(firstClass.Math(20)); //call the method, passing int 20 to it
+            Console.ReadLine();
+
+            FirstClass firstClass1 = new FirstClass(); //instantiating new class again
+            Console.WriteLine(firstClass1.Math((decimal)Convert.ToInt32(20.5))); //calling 2nd method, converting decimal to integer
+            Console.ReadLine();
+
+            FirstClass firstClass2 = new FirstClass(); //instantiation
+            Console.WriteLine(firstClass2.Math("10")); //calling method, passing a string of "10"
+            Console.ReadLine();
+
+        }
+    }
+}
