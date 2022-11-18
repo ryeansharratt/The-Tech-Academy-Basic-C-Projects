2e15e9ffe4735a9be34b19a0e8c16eaaf21d0e03
ryeansharratt <ryean.ann@gmail.com>
2022-11-18 10:05:48 -0700

    Method Assignment Submission
    C# course, module 4

---
+++ MethodAssignment/MethodAssignment/FirstClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class FirstClass
+    {
+        public int Math(int num1, int num2 = 2) //method, 2 int parameters, 1 optional
+        {
+            return num1 + num2; //math operation, returns int result
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                FirstClass firstClass = new FirstClass(); //instantiate new class
+                Console.WriteLine("Enter a number: "); //user input 1
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("OPTIONAL Enter a second number: "); //optional user input 2
+                string num2 = Console.ReadLine();
+                
+
+            if (num2 == "")
+            {
+                Console.WriteLine(firstClass.Math(num1));
+                Console.ReadLine();
+            }
+            else //if user doesn't input second number
+            {
+                Console.WriteLine(firstClass.Math(num1, Convert.ToInt32(num2)));
+                Console.ReadLine();
+            }
+        }
+    }
+}
