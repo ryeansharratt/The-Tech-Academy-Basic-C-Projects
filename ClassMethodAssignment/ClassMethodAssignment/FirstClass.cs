0263e41c7f958bb5754979f308bd075943d621c4
ryeansharratt <ryean.ann@gmail.com>
2022-11-18 14:38:32 -0700

    Class Method Assignment Submission
    C# course, module 4

---
+++ ClassMethodAssignment/ClassMethodAssignment/FirstClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+        class FirstClass
+    {
+        public void Math(int num1)
+        {
+            int result = num1 / 2; //takes user input, divides by 2
+            Console.WriteLine(result); //outputs integer
+            Console.ReadLine();
+        }
+
+        public void Math(out int num2, out int num3) //output parameters, also overloading a method
+        {
+            num2 = 20; //will output these numbers when variable values are called in main()
+            num3 = 12;
+            Console.WriteLine("Num2 is {0}", num2);
+            Console.WriteLine("Num3 is {0}", num3);
+        }
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    static class Program //declare this class as static
+    {
+        static void Main(string[] args)
+        {
+            FirstClass firstClass = new FirstClass(); //instantiate class
+
+            Console.WriteLine("User, please enter a number: "); //have user enter a number
+            int num1 = Convert.ToInt32(Console.ReadLine()); 
+
+            firstClass.Math(num1); //calling method on user input num1
+
+            firstClass.Math(out int num2, out int num3); //calling Math method with output parameters, also inlining variable declaration
+            Console.ReadLine();
+        }
+    }
+}
