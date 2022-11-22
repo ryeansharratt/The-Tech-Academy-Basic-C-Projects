416ae602831201c69a81638760a56b2c76f0ce55
ryeansharratt <ryean.ann@gmail.com>
2022-11-21 21:20:46 -0700

    Operators Assignment Submission
    C# course, module 6

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Employee
+    {
+        public int ID { get; set; } //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2) //true/false comparison
+        {
+            if (employee1.ID == employee2.ID) //if first employee id is same as second employee id, it's true
+                return true;
+            else
+                return false;
+            
+        }
+        public static bool operator!= (Employee employee1, Employee employee2) //have to overload this operator too, (in pairs)
+        {
+            return employee1.ID != employee2.ID;
+        }
+        //apparently must also override this Equals(object)?
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        //compiler warning... according to MS this keeps Equals and GetHashCode synchronized?
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(); //new Employee object
+            employee1.ID = 20;
+            employee1.FirstName = "Ryean";
+            employee1.LastName = "Sharratt";
+
+            Employee employee2 = new Employee(); //new Employee object
+            employee2.ID = 13;
+            employee2.FirstName = "Jaden";
+            employee2.LastName = "Letwin";
+
+            Console.ReadLine();
+
+        }
+    }
+}
