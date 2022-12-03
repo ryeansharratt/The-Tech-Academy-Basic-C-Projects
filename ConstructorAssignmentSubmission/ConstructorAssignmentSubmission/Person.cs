41b7bb88f0ca7d6c6afc829d534985d710d8933b
ryeansharratt <ryean.ann@gmail.com>
2022-12-02 17:20:58 -0700

    Constructor Assignment Submission
    C# course, module 7

---
+++ ConstructorAssignmentSubmission/ConstructorAssignmentSubmission/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignmentSubmission
+{
+    public class Person
+    {
+        public int Age;
+        public string Name;
+
+        public Person(string name) : this(20, name) //can take in only the name, and assigns default age of 20 if needed
+        {
+        }
+        public Person(int age, string name) //constructor that takes in age, name
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+    }
+}
---
+++ ConstructorAssignmentSubmission/ConstructorAssignmentSubmission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Ryean"; //constant variable
+            Console.WriteLine("My name will always be {0}.", myName);
+            var hisName = "Jaden"; //variable using keyword var
+            Console.WriteLine(hisName);
+            Console.ReadLine();
+
+            Person x = new Person("Jaden"); //new Person object, assigning a name, not age though...
+            Console.WriteLine($"Name is: {x.Name}, they are at least this old: {x.Age}."); //will take default age because no age is assigned
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
